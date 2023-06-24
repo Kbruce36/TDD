883b9d612dc205526ef8b0713294d97ee64bd18a
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-06-24 23:10:58 +0300

    pytesting classes

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 # test slightly bigger equation
 
-
+"""
 def circle_area(radius):
     return 3.142 * radius**2
 
 
 def test_area():
     assert circle_area(4) == 50.272
+"""
+
+
+class Account:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def add_to_balance(self, amount):
+        self.balance += amount
+        return self.balance
+
+
+account = Account()
+
+
+def test_balance():
+    assert account.add_to_balance(20000) == 20000
